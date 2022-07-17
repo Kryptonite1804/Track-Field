6dff5cd331635a7191887c792a1ec8d2b08ebdbb
Koki Yamada <koki.y2764@gmail.com>
2022-07-17 14:34:39 +0900

    7/17-K1

--- Track&Field/Record-0-ViewController.swift
+++ Track&Field/Record-0-ViewController.swift
@@ -45,6 +45,8 @@ class Record_0_ViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var sleepEndButton: UIButton!
     @IBOutlet weak var tiredRevelButton: UIButton!
     
+    
+    
     let createdDate_Formatter = DateFormatter()  //DP
     var createdDate: String = ""
     var todayMonth: String = ""
@@ -57,70 +59,9 @@ class Record_0_ViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-          /*
-                let body_Array = [["練習場所タイプ","placeType"],["練習評価","practicePoint"],["食事","mealTimeButton"],["就寝時刻","sleepStart"],["起床時刻","sleepEnd"],["疲労度","tiredRevel"]]
-                let placeType_Array = ["トラック","ロード","土","草地","その他"]
-                let practicePoint_Array = ["★★★★★","★★★★☆","★★★☆☆","★★☆☆☆","★☆☆☆☆","☆☆☆☆☆"]
-                let mealTime_Array = ["0回","1回","2回","3回","4回","5回"]
-                let sleepStart_Array = ["20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30","24:00","24:30","25:00","25:30","26:00"]
-                let sleepEnd_Array = ["04:00","04:30","05:00","05:30","06:00","06:30","07:00","07:30","08:00","8:30","9:00","9:30"]
-                let tiredRevel_Array = ["ひどい　1","かなりある　2","普通　3","ややある　4","なし　5",]
-        
-                var detail_Array = [String]()
-                for m in 0 ... body_Array.count - 1 {
-                    let bodyJP_String = body_Array[m][0]
-                    let bodyEN_String = body_Array[m][1]
-                    
-                    if m == 0 {
-                        aboutButton = placeTypeButton
-                        detail_Array = placeType_Array
-                    } else if m == 1 {
-                        aboutButton = practicePonitButton
-                        detail_Array = practicePoint_Array
-                    } else if m == 2 {
-                        aboutButton = mealTimeButton
-                        detail_Array = mealTime_Array
-                    }else if m == 3 {
-                        aboutButton = sleepStartButton
-                        detail_Array = sleepStart_Array
-                    }else if m == 4 {
-                        aboutButton = sleepEndButton
-                        detail_Array = sleepEnd_Array
-                    }else if m == 5 {
-                        aboutButton = tiredRevelButton
-                        detail_Array = tiredRevel_Array
-                    }
-                    
-                    
-                    aboutButton.setTitle(bodyJP_String, for: .normal)
-                    
-                    var action_Array = [UIMenuElement]()
-                    
-                    for n in 0...detail_Array.count - 1 {
-                        
-                        let detail_String = detail_Array[n] //excerciseType_Arrayからひとつ取り出し ex."JOG"
-                        
-                        let action = UIAction(title: "\(detail_String)", image: UIImage(systemName: "pencil"), handler: { _ in //タイトル設定
-                            print("\(bodyEN_String): \(detail_String)") //何が選択されたか表示
-                            self.aboutButton.setTitle("\(detail_String)", for: .normal) //選択されたものをButtonのTitleに
-                        })
-                        
-                        action_Array.append(action)  //上記一連の動作を設定
-                        
-                    }
-                    
-                        let items = UIMenu(options: .displayInline, children: action_Array) //ここでリストの全ボタンを配置
-                    aboutButton.menu = UIMenu(title: "\(bodyJP_String)を選択", children: [items]) //リスト最上部に出てくる小さくて薄い文字
-                    aboutButton.showsMenuAsPrimaryAction = true //アニメーションさせる
-                    
-                    
-                    
-                }
-        */
-        
         //date
         let today = Date()
-        createdDate_Formatter.dateFormat = "yyyy/MM/dd/E"//2022/07/12/日 履歴のための現在時刻の取得
+        createdDate_Formatter.dateFormat = "yyyy/MM/dd/E"//2022/07/12/日 履歴のための現在日時の取得
         createdDate = createdDate_Formatter.string(from: today)
         createdDate_Formatter.dateFormat = "M"
         todayMonth = createdDate_Formatter.string(from: today)
@@ -129,15 +70,29 @@ class Record_0_ViewController: UIViewController, UITextViewDelegate {
         createdDate_Formatter.dateFormat = "E"
         todayYobi = createdDate_Formatter.string(from: today)
         
-        month.text = todayMonth
-        day.text = todayDay
+        let intMonth: Int = Int(todayMonth)!
+        let intDay: Int = Int(todayDay)!
+        
+        if intMonth > 9  {
+            month.text = todayMonth
+        } else  {
+            month.text = "0\(todayMonth)"
+        }
+        
+        if intDay > 9  {
+            day.text = todayDay
+        } else  {
+            day.text = "0\(todayDay)"
+        }
+        
         date.text = "(\(todayYobi))"
         
         print("日時デフォルト値: \(createdDate)")
-        print("月:",todayMonth)
-        print("日:",todayDay)
-        print("曜日:",todayYobi)
+        print("月:",month.text)
+        print("日:",day.text)
+        print("曜日:",date.text)
 
+        
         //design
         let recordMain = [practicemene_picture,placefeild_picture,point_picture,pain_pisture,eatTime_picture,sleep_picture,tired_picture,writing_picture]
         
@@ -149,22 +104,25 @@ class Record_0_ViewController: UIViewController, UITextViewDelegate {
             recordMainNum?.layer.shadowOpacity = 0.25  //影の濃さ
             recordMainNum?.layer.shadowRadius = 4.0 // 影のぼかし量
             recordMainNum?.layer.shadowOffset = CGSize(width: 3.0, height: 3.0) // 影の方向
+            
         }
         
         
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func parctice_record() {
+    @IBAction func practice_record() {
+        
         self.performSegue(withIdentifier: "go-record-1", sender: self)
+        
     }
     
-    @IBAction func place_feild_record() {
-        aboutButton = placeTypeButton
+    @IBAction func place_field_record() {
+        
     }
     
     @IBAction func point_record() {
-        aboutButton = practicePonitButton
+        
     }
     
     @IBAction func pain_record() {
@@ -172,22 +130,23 @@ class Record_0_ViewController: UIViewController, UITextViewDelegate {
     }
     
     @IBAction func eat_time_record() {
-        aboutButton = mealTimeButton
+        
     }
     
     @IBAction func sleep_start_record() {
-        aboutButton = sleepEndButton
+        
     }
     
     @IBAction func sleep_end_record() {
-        aboutButton = sleepEndButton
+        
     }
     
     @IBAction func tired_record() {
-        aboutButton = tiredRevelButton
+        
     }
     
     @IBAction func register() {
+        
     }
     
     /*
