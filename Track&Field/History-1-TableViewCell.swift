448d0dcc2930c9e6084dc7b5f5620e19b496c22e
Koki Yamada <koki.y2764@gmail.com>
2022-07-18 09:58:02 +0900

    TableViewCell.swift作成

---
+++ Track&Field/History-1-TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  History-1-TableViewCell.swift
+//  Track&Field
+//
+//  Created by 山田航輝 on 2022/07/18.
+//
+
+import UIKit
+
+class History_1_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
