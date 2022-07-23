f58f62fbb3c7b4bc63d000b5c962ca3890691b9a
ibukiSano <ibusan2005@gmail.com>
2022-07-23 17:26:11 +0900

    7/23

---
+++ Track&Field/Setting_0_ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Setting_0_ViewController.swift
+//  Track&Field
+//
+//  Created by 佐野生樹 on 2022/07/23.
+//
+
+import UIKit
+
+class Setting_0_ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
