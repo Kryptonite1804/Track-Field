3b7571b7e80179ace6bb9d6761504b4e920e64e4
Koki Yamada <koki.y2764@gmail.com>
2022-08-11 15:24:53 +0900

    8/11K1

--- Track&Field/Analize-0-ViewController.swift
+++ Track&Field/Analize-0-ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Analize_0_ViewController: UIViewController {
+class Analize_0_ViewController: UIViewController, UITextViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var startday_check: UIImageView!
     @IBOutlet weak var endday_check: UIImageView!
@@ -19,6 +19,31 @@ class Analize_0_ViewController: UIViewController {
     @IBOutlet weak var graphKey1_picture: UIImageView!
     @IBOutlet weak var graphKey2_picture: UIImageView!
     
+    @IBOutlet var startDate_DatePicker: UIDatePicker!
+    @IBOutlet var endDate_DatePicker: UIDatePicker!
+
+    @IBOutlet weak var element1_TF: UITextField!
+    @IBOutlet weak var element2_TF: UITextField!
+    
+    @IBOutlet weak var scrollViewBottomConstraints: NSLayoutConstraint!  //scrollview_キーボード_ずらす
+    
+    var element1_PV = UIPickerView()
+    var element2_PV = UIPickerView()
+    
+    var element1_String: String = ""
+    var element2_String: String = ""
+    
+    
+    
+    var element1_Array = ["曜日","練習場所タイプ","練習評価","痛みの有無","痛みの度合い","食事の回数","睡眠時間","疲労度","チーム","練習タイプ","アップの距離","ダウンの距離","アップのタイム","ダウンのタイム","トータル距離"]
+    var element2_Array = ["曜日","練習場所タイプ","練習評価","痛みの有無","痛みの度合い","食事の回数","睡眠時間","疲労度","チーム","練習タイプ","アップの距離","ダウンの距離","アップのタイム","ダウンのタイム","トータル距離"]
+    
+    var error_Array = ["エラー"]
+    
+    let date_Formatter = DateFormatter()  //DP
+    
+    var startDate_String: String = ""
+    var endDate_String: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,8 +76,195 @@ class Analize_0_ViewController: UIViewController {
         graphKey2_picture.layer.shadowRadius = 4.0 // 影のぼかし量
         graphKey2_picture.layer.shadowOffset = CGSize(width: 3.0, height: 3.0) // 影の方向
         
+        
+        
+        //Toolbar
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35))
+        let spacelItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.setItems([spacelItem, doneItem], animated: true)
+        
+        //PV
+        let pvArray = [element1_PV,element2_PV]
+        let tfArray = [element1_TF,element1_TF]
+        
+        for n in 0...pvArray.count - 1 {
+            
+            let pv = pvArray[n]
+            let tf = tfArray[n]
+            
+            pv.delegate = self
+            pv.dataSource = self
+            tf?.inputView = pv
+            tf?.inputAccessoryView = toolbar
+            pv.tag = n + 1
+            
+            tf?.tintColor = UIColor.clear
+        }
+        
+        //sleepStartTime_Picker初期値
+//        sleepStart_PV.selectRow(12, inComponent: 0, animated: false)
+        
+        
+        
+        //DP
+        date_Formatter.dateFormat = "yyyy/MM/dd"
+        
+        startDate_DatePicker.datePickerMode = UIDatePicker.Mode.date
+        startDate_DatePicker.timeZone = NSTimeZone.local
+        startDate_DatePicker.locale = Locale.current
+        startDate_DatePicker.endEditing(true)
+        
+        endDate_DatePicker.datePickerMode = UIDatePicker.Mode.date
+        endDate_DatePicker.timeZone = NSTimeZone.local
+        endDate_DatePicker.locale = Locale.current
+        endDate_DatePicker.endEditing(true)
+        
+        startDate_DatePicker.maximumDate = NSDate() as Date
+        endDate_DatePicker.maximumDate = NSDate() as Date
+        
+        
+        
+        //scrollview_キーボード_ずらす
+        NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardWillChangeFrame),
+                                                   name: UIResponder.keyboardWillShowNotification,
+                                                   object: nil)
+        NotificationCenter.default.addObserver(self,
+                                                   selector: #selector(keyboardWillHide),
+                                                   name: UIResponder.keyboardWillHideNotification,
+                                                   object: nil)
+        //scrollview_キーボード_ずらす
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    //PV
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            
+            return 1
+            
+    }
+    
+    
+    
+    // UIPickerViewの行数、要素の全数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView.tag == 1 {
+            return element1_Array.count
+        } else if pickerView.tag == 2 {
+            return element2_Array.count
+        } else {
+            return error_Array.count
+        }
+        
+    }
+    
+    
+    
+    // UIPickerViewに表示する配列
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        if pickerView.tag == 1 {
+            return element1_Array[row]
+        } else if pickerView.tag == 2 {
+            return element2_Array[row]
+        } else {
+            return error_Array[row]
+        }
+    }
+    
+    
+    
+    // UIPickerViewのRowが選択された時の挙動
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // 処理
+        
+        if pickerView.tag == 1 {
+            
+            element1_String = element1_Array[row]
+            element1_TF.text = element1_String
+            print("placeType: ",element1_String)
+            
+        } else if pickerView.tag == 2 {
+            
+            element2_String = element2_Array[row]
+            element2_TF.text = element2_String
+            print("practicePoint: ",element2_String)
+            
+        }
+        
+    }
+    
+    
+    
+    @objc func done() {
+        self.view.endEditing(true)
+    }
+    
+    
+    //scrollview_キーボード_ずらす_ここから
+    @objc private func keyboardWillChangeFrame(_ notification: Notification) {
+        print("キーボード表示")
+        
+        //キーボードのサイズ
+  guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+        //キーボードのアニメーション時間
+        let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+        //キーボードのアニメーション曲線
+        let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt,
+        //Outletで結び付けたScrollViewのBottom制約
+        let scrollViewBottomConstraint = self.scrollViewBottomConstraints else { return }
+
+  //キーボードの高さ
+  let keyboardHeight = keyboardFrame.height
+  //Bottom制約再設定
+  scrollViewBottomConstraint.constant = keyboardHeight - 93
+
+  //アニメーションを利用してキーボードが上がるアニメーションと同じ速度でScrollViewのたBottom制約設定を適応
+  UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+    self.view.layoutIfNeeded()
+  })
+      }
+    
+    
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        print("キーボード非表示")
+        
+        //キーボードのアニメーション時間
+            guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
+                  //キーボードのアニメーション曲線
+                  let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt,
+                  //Outletで結び付けたScrollViewのBottom制約
+                  let scrollViewBottomConstraint = self.scrollViewBottomConstraints else { return }
+
+            //画面いっぱいになるのでBottomのマージンを0に戻す
+            scrollViewBottomConstraint.constant = 0
+
+            //アニメーションを利用してキーボードが上がるアニメーションと同じ速度でScrollViewのたBottom制約設定を適応
+            UIView.animate(withDuration: duration, delay: 0, options: UIView.AnimationOptions(rawValue: curve), animations: {
+              self.view.layoutIfNeeded()
+            })
+          
+        
+      }
+    
+    //scrollview_キーボード_ずらす_ここまで
+    
+    
+    
     
     @IBAction func graph_key1() {
     }
@@ -60,17 +272,38 @@ class Analize_0_ViewController: UIViewController {
     @IBAction func graph_key2() {
     }
     
-    @IBAction func startay() {
+    @IBAction func startDate_DP_Tapped() {
+        
+        startDate_String = date_Formatter.string(from: startDate_DatePicker.date)
+        print("開始日時設定: \(startDate_String)")
+        
+        let startDate_Date = date_Formatter.date(from: startDate_String)
+        endDate_DatePicker.minimumDate = startDate_Date
+        
     }
     
-    @IBAction func endday() {
+    @IBAction func endDate_DP_Tapped() {
+        
+        
+        endDate_String = date_Formatter.string(from: endDate_DatePicker.date)
+        print("終了日時設定: \(endDate_String)")
+        
+        let endDate_Date = date_Formatter.date(from: endDate_String)
+        startDate_DatePicker.maximumDate = endDate_Date
+        
     }
+    
+    
     
     @IBAction func make_graph() {
         //グラフの日付・要素確認
         
         self.performSegue(withIdentifier: "go-ana_1", sender: self)
     }
+    
+    
+    
+    
     
     
     /*
