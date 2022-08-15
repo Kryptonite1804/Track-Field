9f368bf12723455aedf92ec09463752be5baf827
ibukiSano <ibusan2005@gmail.com>
2022-08-15 21:34:19 +0900

    会議

--- Track&Field/TabBarController.swift
+++ Track&Field/TabBarController.swift
@@ -14,8 +14,6 @@ class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         // Do any additional setup after loading the view.
     }
     
